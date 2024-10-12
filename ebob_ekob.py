c96f3c00dbc7682af981fd9d5638c901e828f5bc
anuska <150392929+anyskaa@users.noreply.github.com>
2024-10-12 14:21:07 +0400

    Create ebob_ekob.py

---
+++ ebob_ekob.py
@@ -0,0 +1,16 @@
+
+birincisayi = int(input("Birinci sayını daxil et: ")) ikincisayi= int(input("İkinci sayını daxil et"))
+if (birincisayi > ikincisayi) :
+else:
+kucuksayi
+kucuksayi
+=
+ikincisayi
+birincisayi
+for i in range (1, kucuksayi+1):
+if (birincisayi % i==0) and (ikincisayi%i ==0):
+ebob = i
+ekok (birincisayi ikincisayi)//ebob
+=
+print("ebob", ebob)
+print("ekob", ekok)
